08fe776d98cf466a730940fa687e3b342e5e6e9a
Crowdbotics <team@crowdbotics.com>
2020-10-18 22:18:53 +0000

    Update models

--- backend/happy_memories_21685/wsgi.py
+++ backend/happy_memories_21685/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'happy_memories_21685.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "happy_memories_21685.settings")
 
 application = get_wsgi_application()
